13551707eba62dae55dfc9a55a3176346d9a0278
Antonov333 <antonovsergei333@yahoo.com>
2023-03-24 23:16:49 +0300

    Added Main class and gitignore file to Git

---
+++ src/sky/java/course1/Main.java
@@ -0,0 +1,8 @@
+import sky.java.course1.CP1base;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nHi!\n\nJust started with Course Project 1");
+        CP1base.main(null);
+    }
+}
