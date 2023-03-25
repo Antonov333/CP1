3ccc3406a3245b66f1bbf5c1a779e1d9a31164a2
Antonov333 <antonovsergei333@yahoo.com>
2023-03-25 23:32:01 +0300

    Base functions implemented

--- src/sky/java/course1/CP1base.java
+++ src/sky/java/course1/CP1base.java
@@ -6,15 +6,13 @@ public class CP1base {
     public static Department[] depts = new Department[6];
 
     public static void main(String[] args) {
-        System.out.println("This is Course Project 1,  basic level");
-
-        System.out.println("employeeCounter:" + employeeCounter);
+        System.out.println("\nCOURSE PROJECT 1\n" +
+                             "----------------");
 
         Person john = new Person("John", "Peter", "Smith");
         Person anna = new Person("Anna", "Petrovna", "Ivanova");
         Person tatyana = new Person("Tatyana", "Fyodorovna", "Belova");
         Person ketsuro = new Person("Ketsuro", "Kaoru", "Fujiwara");
-        System.out.println(john + "\n" + anna + "\n" + tatyana + "\n" + ketsuro);
 
         Employee boss = new Employee(1, john, 1, 199999);
         Employee officeManager = new Employee(2, anna, 1, 60000);
@@ -26,36 +24,27 @@ public class CP1base {
         enroll(salesManager);
         enroll(productionManager);
 
-        for (int i = 0; i < employeeCounter; i++) {
-            System.out.println(personnel[i]);
-        }
-
-        System.out.println("Max salary pers ID: " + maxSalaryPersID());
-
-
         String[] deptNames = new String[]{"Projected", "Management", "Sales", "Production", "Delivery", "Marketing"};
 
         for (int i = 0; i <= depts.length - 1; i++) {
             depts[i] = DepartmentService.setDepartment(i, deptNames[i]);
         }
-    /**
-        for (int i = depts.length - 1; i >= 0; i--) {
-            System.out.println("i=" + i + " " + depts[i]);
-        } */
 
         printMaxSalaryInfo();
-        System.out.println(personnel[3].getSalary());
-        System.out.println(personnel[maxSalaryPersID() - 1].getSalary() >= personnel[3].getSalary());
+        printMinimumSalaryInfo();
+        printAverageSalary();
+        printPersonnelInfo();
+        printPersonnelNames();
+
 
     }
 
     public static void enroll(Employee candidate) {
         personnel[employeeCounter] = candidate;
         employeeCounter++;
-        System.out.println("Enrolled: " + personnel[employeeCounter - 1]);
     }
 
-    public static int maxSalaryPersID() {
+    public static int getMaxSalaryPersID() {
         int n = 0,
                 maxSalary = 0;
         for (int i = 0; i <= employeeCounter - 1; i++) {
@@ -67,11 +56,60 @@ public class CP1base {
         return n;
     }
 
+    public static int getMinSalaryPersID() {
+        int n = 0,
+                minSalary = personnel[0].getSalary();
+        for (int i = 1; i <= employeeCounter - 1; i++) {
+            if (minSalary > personnel[i].getSalary()) {
+                n = i + 1;
+                minSalary = personnel[i].getSalary();
+            }
+        }
+        return n;
+    }
+
+    public static void printMinimumSalaryInfo() {
+        int i = getMinSalaryPersID() - 1;
+        System.out.println("\nMinimum salary of " + personnel[i].getSalary() +
+                " earned " + personnel[i].getPerson() + " of " +
+                depts[personnel[i].getDeptId()].getName() + " department");
+    }
+
+
     public static void printMaxSalaryInfo() {
-        int i = maxSalaryPersID() - 1;
+        int i = getMaxSalaryPersID() - 1;
         System.out.println("\nMaximum salary of " + personnel[i].getSalary() +
                 " achieved " + personnel[i].getPerson() + " of " +
                 depts[personnel[i].getDeptId()].getName() + " department");
+    }
+
+    public static void printPersonnelInfo() {
+        System.out.println("\nCOMPANY CREW INFORMATION\n" +
+                             "------------------------");
+        for (int i=0 ; i<=employeeCounter-1 ; i++) {
+            System.out.println(personnel[i].getPersId() + ". " + personnel[i].getPerson() +
+                    "\t\t" + depts[personnel[i].getDeptId()] + "\tSalary: " + personnel[i].getSalary());
+        }
+    }
+
+    public static void printPersonnelNames() {
+        System.out.println("\nCOMPANY CREW LIST\n" +
+                "-----------------");
+        for (int i=0 ; i<=employeeCounter-1 ; i++) {
+            System.out.println(personnel[i].getPersId() + ". " + personnel[i].getPerson());
+        }
+
+    }
+    public static float calcAverageSalary(){
+        int m =0 ;
+        for (int i = 0 ; i < employeeCounter ; i++ ){
+            m+=personnel[i].getSalary() ;
+        }
+        return (float) m / (float) employeeCounter;
+    }
+
+    public static void printAverageSalary() {
+        System.out.println("\nAverage salary in company is " + calcAverageSalary());
     }
 
 }
